c29d14223bfa0a7d44dc3a4fbefdfac4068e9f65
Trung Nguyen <trungnguyen2809@gmail.com>
2022-03-31 13:48:25 -0500

    Adding SignUp and LogIn features - create model for User documents - add new routes for SignUp and LogIn - add  authenController to take care of authencication

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const userController = require(`${__dirname}/../controllers/userController`);
+const authenController = require(`${__dirname}/../controllers/authenController`);
 
 const router = express.Router();
+
+router.route("/signup").post(authenController.signup);
+router.route("/login").post(authenController.login);
 
 router.route("/").get(userController.getUser);
 
