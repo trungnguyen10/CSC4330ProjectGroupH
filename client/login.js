2b6ef757e68f5fc465e983f0dd13f66b901ab83d
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-04-03 18:28:26 -0500

    Working on Login/Signup frontend

---
+++ client/login.js
@@ -0,0 +1,39 @@
+const axios = require('axios');
+function submitForm()
+{
+  console.log("Submitting the form");
+  var datajs = {};
+  try
+  {
+     datajs.email = document.getElementById('email').value;
+     datajs.password = document.getElementById('password').value;
+  }
+  catch
+  {}
+  console.log(datajs);
+  const options = {
+    url: 'http://localhost:3000/api/login',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: {
+      email: datajs.email,
+      password: datajs.password
+    }
+  };
+  axios(options)
+  .then(response => {
+    console.log(response.status);
+  }); 
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    // do something
+  var btn = document.getElementById('submit');
+
+// attach anonymous function to click event
+btn.addEventListener('click', submitForm);
+});
---
+++ client/signup.js
@@ -0,0 +1,52 @@
+const axios = require('axios').default;
+function submitForm()
+{
+  console.log("Submitting the form");
+  var datajs = {};
+  try
+  {
+     datajs.first = document.getElementById('First').value;
+     datajs.last = document.getElementById('Last').value;
+     datajs.email = document.getElementById('email').value;
+     datajs.password = document.getElementById('password').value;
+     datajs.confirm = document.getElementById('confirm').value;
+  }
+
+  
+
+  catch
+  {}
+  console.log(datajs);
+  if(datajs.password !== datajs.confirm)
+  {
+    alert("Password does not match");
+  }
+  const options = {
+    url: 'http://localhost:3000/api/signup',
+    method: 'POST',
+    headers: {
+      'Accept': 'application/json',
+      'Content-Type': 'application/json;charset=UTF-8'
+    },
+    data: {
+      first: datajs.first,
+      last: datajs.last,
+      email: datajs.email,
+      password: datajs.password,
+      confirm: datajs.confirm
+    }
+  };
+  axios(options)
+  .then(response => {
+    console.log(response.status);
+  }); 
+
+}
+
+document.addEventListener('DOMContentLoaded', function(){
+    // do something
+  var btn = document.getElementById('submit');
+
+// attach anonymous function to click event
+btn.addEventListener('click', submitForm);
+});
