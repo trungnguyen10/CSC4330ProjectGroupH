d809092ac290f3c8003f19c01bc949eb63bdad67
AndresOberhelman <andres.w.oberhelman@gmail.com>
2022-04-03 18:41:48 -0500

    Test

--- index.js
+++ index.js
@@ -8,5 +8,16 @@ app.use(express.json());
 
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/listing", listingRoute);
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/signup', (req, res) => {
+    console.log(req.body)
+      .then((data) => {
+        res.status(201).send(data);
+      })
+      .catch((err) => {
+        res.sendStatus(500);
+      });
+  });
 
 module.exports = app;
