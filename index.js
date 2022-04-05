6a9ae68a3586e64e1902a55b1f279e677d607d15
Trung Nguyen <trungnguyen2809@gmail.com>
2022-04-05 11:33:38 -0500

    quickfix for login, need to re-organize

--- index.js
+++ index.js
@@ -1,36 +1,53 @@
 const express = require("express");
+const helmet = require("helmet");
 const userRoute = require(`${__dirname}/routes/userRoute`);
 const listingRoute = require(`${__dirname}/routes/listingRoute`);
-const fs = require('fs');
+const fs = require("fs");
 
 const app = new express();
 
+// set view engine
+app.set("view engine", "pug");
+app.set("views", `${__dirname}/views`);
+
+// ADDING MIDDLEWARE
+// serving static files
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(`${__dirname}/client`));
+
+// set security HTTP headers
+app.use(helmet());
+
+// body parsers
 app.use(express.json());
+
+// ROUTES
+app.get("/", (req, res) => {
+  res.status(200).render("base");
+});
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/listing", listingRoute);
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static('client'));
 
-
-app.get('/', (req, res) => {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-    fs.readFile("./client/login.html", null, function(error, data) {
-      if(error) { 
-        res.writeHead(404);
-        res.write("File not Found");
-      }
-      else {
-        res.write(data);
-        console.log("File found");
-      }
-      res.end();
-    });
-    console.log('Starting the home page');
+app.get("/", (req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  fs.readFile("./client/login.html", null, function (error, data) {
+    if (error) {
+      res.writeHead(404);
+      res.write("File not Found");
+    } else {
+      res.write(data);
+      console.log("File found");
+    }
+    res.end();
+  });
+  console.log("Starting the home page");
 });
 
-app.get("/signup", (req, res) => {
-}).post('/signup', (req, res) => {
-    console.log(req.body)
+app
+  .get("/signup", (req, res) => {})
+  .post("/signup", (req, res) => {
+    console
+      .log(req.body)
       .then((data) => {
         res.status(201).send(data);
       })
