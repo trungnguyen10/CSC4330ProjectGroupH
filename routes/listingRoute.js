66b10287326c4b49865fd1bbb706c746d1219ee7
Blake Opial <blakopial@gmail.com>
2022-04-07 23:09:59 -0500

    Early code for Filtering

--- routes/listingRoute.js
+++ routes/listingRoute.js
@@ -8,7 +8,10 @@ router
   .get(authenController.protect, listingController.getAllListings)
   .post(listingController.createListing);
 
-router.route("/:id").get(listingController.getListing);
-//.patch(listingController.updateListing)
-//.delete(listingController.deleteListing);
+  router.route("/sortByTime")
+  .get(authenController.protect, sort(listingController.getAllListings));
+router.route("/:id")
+.get(listingController.getListing)
+.patch(listingController.updateListing)
+.delete(listingController.deleteListing);
 module.exports = router;
