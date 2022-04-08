29d5ff5dc59ffd7b9768b053dbd425457d883b22
Trung Nguyen <trungnguyen2809@gmail.com>
2022-04-07 19:09:39 -0500

    implement error handling

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const helmet = require("helmet");
+const cookieParser = require("cookie-parser");
+const AppError = require(`${__dirname}/utils/appError`);
+const globalErrorHandler = require(`${__dirname}/controllers/errorController`);
 const userRoute = require(`${__dirname}/routes/userRoute`);
 const listingRoute = require(`${__dirname}/routes/listingRoute`);
+const viewRoute = require(`${__dirname}/routes/viewRoute`);
+
 const fs = require("fs");
 
 const app = new express();
@@ -20,40 +25,45 @@ app.use(helmet());
 
 // body parsers
 app.use(express.json());
+app.use(cookieParser());
 
 // ROUTES
-// app.get("/", (req, res) => {
-//   res.status(200).render("base");
-// });
+app.use("/", viewRoute);
 app.use("/api/v1/user", userRoute);
 app.use("/api/v1/listing", listingRoute);
 
-app.get("/", (req, res) => {
-  res.writeHead(200, { "Content-Type": "text/html" });
-  fs.readFile("./client/login.html", null, function (error, data) {
-    if (error) {
-      res.writeHead(404);
-      res.write("File not Found");
-    } else {
-      res.write(data);
-      console.log("File found");
-    }
-    res.end();
-  });
-  console.log("Starting the home page");
+// app.get("/", (req, res) => {
+//   res.writeHead(200, { "Content-Type": "text/html" });
+//   fs.readFile("./client/login.html", null, function (error, data) {
+//     if (error) {
+//       res.writeHead(404);
+//       res.write("File not Found");
+//     } else {
+//       res.write(data);
+//       console.log("File found");
+//     }
+//     res.end();
+//   });
+//   console.log("Starting the home page");
+// });
+
+// app
+//   .get("/signup", (req, res) => {})
+//   .post("/signup", (req, res) => {
+//     console
+//       .log(req.body)
+//       .then((data) => {
+//         res.status(201).send(data);
+//       })
+//       .catch((err) => {
+//         res.sendStatus(500);
+//       });
+//   });
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
 });
 
-app
-  .get("/signup", (req, res) => {})
-  .post("/signup", (req, res) => {
-    console
-      .log(req.body)
-      .then((data) => {
-        res.status(201).send(data);
-      })
-      .catch((err) => {
-        res.sendStatus(500);
-      });
-  });
+app.use(globalErrorHandler);
 
 module.exports = app;
