d17b69e3d633046cd44d6ed87347bb05aa2856cf
trung <trungnguyen2809@gmail.com>
2022-04-22 07:45:52 -0500

    error handling in userController

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,10 @@
 const User = require(`${__dirname}/../models/userModel`);
+const catchAsync = require(`${__dirname}/../utils/catchAsync`);
 
-exports.getUser = async function (req, res) {
+exports.getUser = catchAsync(async function (req, res) {
   const user = await User.findById(req.user._id);
   res.status(200).json({
     status: "success",
     data: user,
   });
-};
+});
