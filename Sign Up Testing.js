918fb548f5e1b1dc3e33f8b4bdcf68da5b4c4de8
DPardo7 <99456649+DPardo7@users.noreply.github.com>
2022-04-24 18:26:52 -0500

    Update Sign Up Testing.js

--- Sign Up Testing.js
+++ Sign Up Testing.js
@@ -1,31 +1,14 @@
-process.env.NODE_ENV = 'test';
+const assert = require('chai').assert;
+const signUp = require('../test/signupPractice').signUp;
 
-let mongoose = require("mongoose");
-let signup = require('../client/js/signup.js');
+describe('Signup', function() {
+    it('Signup should allow you to fill in name, email, password, passwordConfimed', function() {
+        let result = signUp();
+        assert.equal(result, 'name, email, password, passwordConfimed');
+    });
 
-//Require the dev-dependencies
-let chai = require('chai');
-let chaiHttp = require('chai-http');
-let server = require('../server');
-let should = chai.should();
-
-
-chai.use(chaiHttp);
-//Our parent block
-const isInteger = require("./isInteger");
-
-describe("isInteger", () => {
-    const integerNumbers = [-10, -1, 0, 1, 10];
-
-    test.each(integerNumbers)(
-        "passes for integer value %j",
-        (fixture) => expect(isInteger(fixture)).toBe(true)
-    );
-
-    const floatNumbers = [-10.1, -1.1, 0.1, 1.1, 10.1];
-
-    test.each(floatNumbers)(
-        "fails for non-integer value %j",
-        (fixture) => expect(isInteger(fixture)).toBe(false)
-    );
+    it('Signup should allow for type string', function() {
+        let result = signUp();
+        assert.typeOf(result, string);
+    });
 });
