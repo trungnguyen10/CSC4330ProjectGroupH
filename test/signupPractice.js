60fa6e74966796faac6b384c3befdce8fec71ed9
DPardo7 <99456649+DPardo7@users.noreply.github.com>
2022-04-25 23:38:01 -0500

    Update signupPractice.js

--- test/signupPractice.js
+++ test/signupPractice.js
@@ -1,5 +1,13 @@
 module.exports = {
     signUp: function(){
-        return 'name, email, password, passwordConfimed'
+        return 'name, email, password, passwordConfimed';
+    },
+
+    signupEmail: function() {
+        return 'jshepard@columbus.edu';
+    },
+
+    signupPassword: function() {
+        return 'password';
     }
 }
