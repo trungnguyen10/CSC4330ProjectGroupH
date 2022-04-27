b055b82a8de9d0b49669e381fb3095b79adba73a
trung <trungnguyen2809@gmail.com>
2022-04-27 18:50:03 -0500

    add wishlists var to profile.pug

--- controllers/viewController.js
+++ controllers/viewController.js
@@ -1,6 +1,7 @@
 const catchAsync = require(`${__dirname}/../utils/catchAsync`);
 const AppError = require(`${__dirname}/../utils/appError`);
 const Listing = require(`${__dirname}/../models/listingModel`);
+const WishList = require(`${__dirname}/../models/wishlistModel`);
 
 exports.getHomePage = catchAsync(async function (req, res, next) {
   const listings = await Listing.find().sort("-createAt");
@@ -19,9 +20,9 @@ exports.getSignupForm = function (req, res) {
 exports.getProfileForm = catchAsync(async function (req, res, next) {
   const userId = req.user._id + "";
   const listings = await Listing.find({ belongTo: userId }).sort("-createAt");
+  const wishlists = await WishList.find({ user_id: userId });
   console.log(req.user);
   console.log(listings);
-  res.status(200).render("profile", { user: req.user, listings });
+  console.log(wishlists);
+  res.status(200).render("profile", { user: req.user, listings, wishlists });
 });
-
-
