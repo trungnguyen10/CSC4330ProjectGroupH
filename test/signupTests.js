17d1c028a7948ffeef17d596632b4e508412dec2
DPardo7 <99456649+DPardo7@users.noreply.github.com>
2022-04-26 21:19:54 -0500

    Update signupTests.js

--- test/signupTests.js
+++ test/signupTests.js
@@ -1,3 +1,17 @@
+// submit these items under the terminal:
+// Npm install mocha
+// Npm install chai
+// Make sure that mocha, chai, and jest are part of dependencies and other sets in the package.json and package-lock.json
+// Next, you would have to put “test”: ”mocha” under the “scripts” section of the .json files
+// Create a test java file within the coding program
+// Set up the code so that it could be read through chai and mocha in node
+// Such as bringing in should, expect, and assert as a constant for chai; and describe, it, and other terms that deal with testing for mocha
+// Now, put “npm test” or “npm run test” in the terminal
+// There the user will see if their test(s) is/are passing or failed
+// Following that, the user can either add more tests or fix those that failed
+
+
+
 const { signupEmail, signupPassword } = require('../test/signupPractice');
 const assert = require('chai').assert;
 const signUp = require('../test/signupPractice').signUp;
@@ -36,3 +50,14 @@ describe('Signup', function() {
         assert.equal(loginHere, "./login.html");
     });
 });
+
+// > csc4330projectgrouph@1.0.0 test
+// > mocha
+// Signup
+// ✔ Signup should allow you to fill in name, email, password, passwordConfimed
+// ✔ Signup should return type string
+// ✔ Signup should have be a student email
+// ✔ Password should have 8 characters at most in order to be accepted
+// ✔ Confirmed password should completely relate to password
+// ✔ The button at the bottom of the signup page should bring the user to the login
+// 6 passing (31ms)
